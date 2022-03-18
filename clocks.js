51c3de7018dc95fe024be90f8099b14b2a5f7401
Alexandra Corremans <alexandra.corremans@gmail.com>
2022-03-18 14:29:59 +1100

    refactor

--- clocks.js
+++ clocks.js
@@ -1,73 +1,124 @@
+// set data
+const timeZones = {
+  0: {
+    timezone: "America/Los_Angeles",
+    name: "West Coast",
+    team: "Emma, Nick, Sri"
+  },
+  1: {
+    timezone: "America/New_York",
+    name: "East Coast",
+    team: "Jeff, Dan, Rich, Scott"
+  },
+  2: {
+    timezone: "Europe/London",
+    name: "London",
+    team: "Lewis"
+  },
+  3: {
+    timezone: "Europe/Berlin",
+    name: "Berlin",
+    team: "Michal"
+  },
+  4: {
+    timezone: "Asia/Dubai",
+    name: "Dubai",
+    team: "Garima"
+  },
+  5: {
+    timezone: "Australia/Sydney",
+    name: "Sydney",
+    team: "Alex"
+  }
+}
+
+const numberOfTimezones = Object.keys(timeZones).length;
+
+// import luxon
 var DateTime = luxon.DateTime;
 
+// helpers
 function generateDT(tzString) {
-    return DateTime.now().setLocale('en-GB').setZone(tzString);
+  return DateTime.now().setLocale('en-GB').setZone(tzString);
 }
 
 function getClass(hour) {
-    if (hour >=6 && hour < 8) {
-        return "dawn";
-    } else if (hour >=8 && hour < 10) {
-        return "morning";
-    } else if (hour >=10 && hour < 12) {
-        return "late-morning";
-    } else if (hour >=12 && hour < 14) {
-        return "lunch";
-    } else if (hour >=14 && hour < 16) {
-        return "afternoon";
-    } else if (hour >=16 && hour < 18) {
-        return "late-afternoon";
-    } else if (hour >=18 && hour < 20) {
-        return "sunset";
-    } else if (hour >=20 && hour < 22) {
-        return "evening";
-    } else {
-        return "night";
-    }
+  if (hour >=6 && hour < 8) {
+      return "dawn";
+  } else if (hour >=8 && hour < 10) {
+      return "morning";
+  } else if (hour >=10 && hour < 12) {
+      return "late-morning";
+  } else if (hour >=12 && hour < 14) {
+      return "lunch";
+  } else if (hour >=14 && hour < 16) {
+      return "afternoon";
+  } else if (hour >=16 && hour < 18) {
+      return "late-afternoon";
+  } else if (hour >=18 && hour < 20) {
+      return "sunset";
+  } else if (hour >=20 && hour < 22) {
+      return "evening";
+  } else {
+      return "night";
+  }
 }
 
+function generateHTML(i, name, team) {
+  const outerDiv = document.createElement('div');
+  outerDiv.classList.add("col-sm", "text-center", "d-flex", "flex-column", "justify-content-between");
+
+  const topRow = document.createElement('div');
+  topRow.classList.add("row");
+  outerDiv.appendChild(topRow);
+  
+  const middleRow = document.createElement('div');
+  middleRow.classList.add("row", "mt-3", "mt-sm-0");
+  
+  const clock = document.createElement('h1');
+  clock.classList.add("clock");
+  clock.id = `time-${i}`
+  middleRow.appendChild(clock);
+
+  const date = document.createElement('p');
+  date.id = `date-${i}`
+  middleRow.appendChild(date);
+  outerDiv.appendChild(middleRow);
+
+  const bottomRow = document.createElement('div');
+  bottomRow.classList.add("row", "mb-1", "mb-sm-4");
+  const bottomRowHTML = `
+    <p><span>${name}</span>
+      <br><span id="offset-${i}"></span>
+      <br><span>${team}</span>
+    </p>
+  `;
+  bottomRow.innerHTML = bottomRowHTML;
+  outerDiv.appendChild(bottomRow);
+
+  const main = document.querySelector('main');
+  main.appendChild(outerDiv);
+}
+
+function displayClock(i, timezone) {
+  const dt = generateDT(timezone);
+  document.getElementById(`time-${i}`).textContent = dt.toLocaleString(DateTime.TIME_SIMPLE);
+  document.getElementById(`date-${i}`).textContent = dt.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
+  document.getElementById(`offset-${i}`).textContent = dt.offsetNameShort;
+  document.getElementById(`time-${i}`).parentElement.parentElement.classList.remove(getClass(dt.hour - 2));
+  document.getElementById(`time-${i}`).parentElement.parentElement.classList.add(getClass(dt.hour));
+}
+
+// main function, runs every second
 function displayClocks() {
-    const pst = generateDT("America/Los_Angeles");
-    document.getElementById("date-0").innerHTML = pst.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-0").innerHTML = pst.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-0").innerHTML = pst.offsetNameShort;
-    document.getElementById("time-0").parentElement.parentElement.classList.remove(getClass(pst.hour - 2));
-    document.getElementById("time-0").parentElement.parentElement.classList.add(getClass(pst.hour));
-
-    const est = generateDT("America/New_York");
-    document.getElementById("date-1").innerHTML = est.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-1").innerHTML = est.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-1").innerHTML = est.offsetNameShort;
-    document.getElementById("time-1").parentElement.parentElement.classList.remove(getClass(est.hour - 2));
-    document.getElementById("time-1").parentElement.parentElement.classList.add(getClass(est.hour));
-
-    const uk = generateDT("Europe/London");
-    document.getElementById("date-2").innerHTML = uk.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-2").innerHTML = uk.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-2").innerHTML = uk.offsetNameShort;
-    document.getElementById("time-2").parentElement.parentElement.classList.remove(getClass(uk.hour - 2));
-    document.getElementById("time-2").parentElement.parentElement.classList.add(getClass(uk.hour));
-
-    const eur = generateDT("Europe/Berlin");
-    document.getElementById("date-3").innerHTML = eur.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-3").innerHTML = eur.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-3").innerHTML = eur.offsetNameShort;
-    document.getElementById("time-3").parentElement.parentElement.classList.remove(getClass(eur.hour - 2));
-    document.getElementById("time-3").parentElement.parentElement.classList.add(getClass(eur.hour));
-
-    const dub = generateDT("Asia/Dubai");
-    document.getElementById("date-4").innerHTML = dub.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-4").innerHTML = dub.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-4").innerHTML = dub.offsetNameShort;
-    document.getElementById("time-4").parentElement.parentElement.classList.remove(getClass(dub.hour - 2));
-    document.getElementById("time-4").parentElement.parentElement.classList.add(getClass(dub.hour));
-
-    const syd = generateDT("Australia/Sydney");
-    document.getElementById("date-5").innerHTML = syd.toLocaleString({ weekday: 'short', month: 'long', day: 'numeric' });
-    document.getElementById("time-5").innerHTML = syd.toLocaleString(DateTime.TIME_SIMPLE);
-    document.getElementById("offset-5").innerHTML = syd.offsetNameShort;
-    document.getElementById("time-5").parentElement.parentElement.classList.remove(getClass(syd.hour - 2));
-    document.getElementById("time-5").parentElement.parentElement.classList.add(getClass(syd.hour));
+  for (var i = 0; i < numberOfTimezones; ++i) {
+    // first time it runs, create HTML structure
+    if (document.querySelector('main').childElementCount < numberOfTimezones) {
+      generateHTML(i, timeZones[i].name, timeZones[i].team);
+    }
+    // display dynamic data
+    displayClock(i, timeZones[i].timezone)
+  }
 }
 
 window.onload = displayClocks();
